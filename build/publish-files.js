c409409c2d9aa4b3a7e2687902d2c56711ae552d
ovrmrw <t.ngc.0403@gmail.com>
2016-10-07 22:40:27 +0900

    Fix

--- build/publish-files.js
+++ build/publish-files.js
@@ -3,12 +3,16 @@ const path = require('path');
 
 
 const root = path.resolve();
-const DIST = path.join(root, 'dist');
-const DOCS = path.join(root, 'docs');
+const DIST = path.resolve(root, 'dist');
+const DOCS = path.resolve(root, 'docs');
+const NGFACTORY = path.resolve(DOCS, 'ngfactory');
 
-fs.remove(DOCS, err => {
+fs.remove(DOCS, err => { /* delete ./docs directory. */
   if (err) { throw err; }
-  fs.copy(DIST, DOCS, err => {
+  fs.copy(DIST, DOCS, err => { /* copy ./dist directory to ./docs */
     if (err) { throw err; }
+    fs.remove(NGFACTORY, err => { /* delete ./docs/ngfactory directory. */
+      if (err) { throw err; }
+    });
   });
 });
