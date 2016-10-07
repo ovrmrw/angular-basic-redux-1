f18d5d4005ed0ba8ffc6e301e69d4a401f6bfe33
ovrmrw <t.ngc.0403@gmail.com>
2016-10-07 15:27:06 +0900

    Update

---
+++ build/publish-files.js
@@ -0,0 +1,14 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+
+const root = path.resolve();
+const DIST = path.join(root, 'dist');
+const PUBLISH = path.join(root, '.publish');
+
+fs.remove(PUBLISH, err => {
+  if (err) { throw err; }
+  fs.copy(DIST, PUBLISH, err => {
+    if (err) { throw err; }
+  });
+});
